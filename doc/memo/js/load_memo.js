b310672ca5c24280f2e235332b2f0f3597725f3e
YotioSoft <ytani0323@gmail.com>
2021-10-26 15:39:49 +0900

    メモの削除ボタン仮設置

--- doc/memo/js/load_memo.js
+++ doc/memo/js/load_memo.js
@@ -8,6 +8,17 @@ if (get_json) {
         memo_title.className    = 'title';
         memo_title.textContent  = memo_array[memo].title;
 
+        var memo_delete_button_link = document.createElement('a');
+        memo_delete_button_link.href = "JavaScript:OnDeleteButtonClick()";
+        var memo_delete_button  = document.createElement('div');
+        memo_delete_button.className = 'round-rect-button';
+        var memo_delete_button_text = document.createElement('p');
+        memo_delete_button_text.className = 'middle-text';
+        memo_delete_button_text.textContent = '削除';
+        memo_delete_button.appendChild(memo_delete_button_text);
+        memo_delete_button.style = 'min-width: 50px; padding: 2px;';
+        memo_delete_button_link.appendChild(memo_delete_button);
+
         var memo_date           = document.createElement('div');
         memo_date.className     = 'date';
         memo_date.textContent   = memo_array[memo].time.year + '/' + zeroPadding(memo_array[memo].time.month, 2) + '/' + zeroPadding(memo_array[memo].time.date, 2) + ' ' 
@@ -20,8 +31,9 @@ if (get_json) {
     
         var memo_paper          = document.createElement('div');
         memo_paper.className    = 'memo-paper';
-        memo_paper.style        = 'background-color: #'+memo_array[memo].color;
+        memo_paper.style        = 'background-color: #'+memo_array[memo].color+';';
         memo_paper.appendChild(memo_title);
+        memo_paper.appendChild(memo_delete_button_link);
         memo_paper.appendChild(memo_date);
         memo_paper.appendChild(memo_text);
 
