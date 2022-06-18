fe4e4d147625d9ca0f5bd5c6bbf8c3c7a729923b
YotioSoft <ytani0323@gmail.com>
2022-06-18 18:58:58 +0900

    revise for RandomAPI

--- random/js/random.js
+++ random/js/random.js
@@ -1,5 +1,74 @@
+function update_output(rand_array) {
+    output = document.getElementById('id_output');
+    output.value = "";
+    for (i=0; i<rand_array.length; i++) {
+        output.value += rand_array[i] + '\n';
+    }
+}
+
+function uniform() {
+    // クエリパラメータの登録
+    const params = {};
+
+    var trials              = document.getElementById('id_trials');
+    var integer_mode        = document.getElementById('id_integer_mode');
+
+    var range               = document.getElementsByName('range');
+    var select_random       = document.getElementById('id_select_random');
+
+    var input_limit_min     = document.getElementById('id_input_min');
+    var input_limit_max     = document.getElementById('id_input_max');
+
+    var input_digit         = document.getElementById('id_input_digit');
+
+    let random;
+
+    if (integer_mode.checked) {
+        params["type"] = "int";
+    }
+    else {
+        params["type"] = "float";
+    }
+
+    params["trials"] = trials.value;
+
+    if (select_random.value == "uniform") {
+        random = Math.random();
+    }
+    else if (select_random.value == "normal") {
+        random = norm_random();
+    }
+ 
+    if (range[0].checked) {
+        params["max"] = parseInt(input_limit_max.value);
+        params["min"] = parseInt(input_limit_min.value);
+    }
+    else if (range[1].checked) {
+        let digits = parseInt(input_digit.value);
+        let base_num = Math.pow(10, digits-1);
+        let max_num = Math.pow(10, digits) - 1;
+        
+        params["max"] = base_num;
+        params["min"] = max_num;
+    }
+    
+    const query = new URLSearchParams(params);
+
+    // JSONをフェッチ
+    fetch(`https://murmuring-taiga-39514.herokuapp.com/random/uniform?${query}`)
+    .then(response => response.json())
+    .then(data => {
+        if (data.hasOwnProperty('rand_array')) {
+            update_output(data.rand_array);     // 結果を出力
+        }
+        else if (data.hasOwnProperty('error_message')) {
+            alert(data.error_message);
+        }
+    });
+}
+
 function norm_random() {
-    var num = 0, i;
+    num = 0, i;
     for (i = 0; i < 12; i++) {
         num += Math.random();
     }
@@ -48,8 +117,5 @@ function make_number() {
 }
 
 function OnMakeButtonClick() {
-    let num_output = document.getElementById('id_output');
-    let random_num = make_number();
-    
-    num_output.value = random_num;
+    uniform();
 }
