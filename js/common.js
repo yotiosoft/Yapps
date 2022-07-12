ffe5d53d33156abec802d710d532b5e5c8c56802
YotioSoft <ytani0323@gmail.com>
2022-07-13 02:37:36 +0900

    making header_m

--- js/common.js
+++ js/common.js
@@ -1,5 +1,25 @@
+// gtag
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+gtag('config', 'G-43EGG0HL47');
+
 //共通パーツ読み込み
 $(function() {
-    $("#header").load("/header.html");
-    $("#footer").load("/footer.html");
+    if (window.innerWidth <= 767 || navigator.userAgent.match(/iPhone|Android.+Mobile/)) {
+        $("#header").load("/header_m.html");
+    }
+    else {
+        $("#header").load("/header.html");
+    }
+    $("#footer").load("/footer_m.html");
+    
+    window.addEventListener('resize', function(){
+        if (window.innerWidth <= 767 || navigator.userAgent.match(/iPhone|Android.+Mobile/)) {
+            $("#header").load("/header_m.html");
+        }
+        else {
+            $("#header").load("/header.html");
+        }
+    });
 });
