755eabb2fa44be1b6aa44bf2db5fa5daa4871a7b
YotioSoft <ytani0323@gmail.com>
2022-07-13 02:46:56 +0900

    fix menu button position

--- js/common.js
+++ js/common.js
@@ -6,7 +6,7 @@ gtag('config', 'G-43EGG0HL47');
 
 //共通パーツ読み込み
 $(function() {
-    if (window.innerWidth <= 767 || navigator.userAgent.match(/iPhone|Android.+Mobile/)) {
+    if (window.innerWidth <= 700 || navigator.userAgent.match(/iPhone|Android.+Mobile/)) {
         $("#header").load("/header_m.html");
     }
     else {
@@ -15,7 +15,7 @@ $(function() {
     $("#footer").load("/footer_m.html");
     
     window.addEventListener('resize', function(){
-        if (window.innerWidth <= 767 || navigator.userAgent.match(/iPhone|Android.+Mobile/)) {
+        if (window.innerWidth <= 700 || navigator.userAgent.match(/iPhone|Android.+Mobile/)) {
             $("#header").load("/header_m.html");
         }
         else {
