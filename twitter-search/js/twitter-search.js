6b52c0c4285fc92bb0c1e82719b4661f1f0ce0ad
YotioSoft <ytani0323@gmail.com>
2022-07-16 00:18:10 +0900

    making twitter-search.js

--- twitter-search/js/twitter-search.js
+++ twitter-search/js/twitter-search.js
@@ -1,95 +1,27 @@
-var input_bin = document.getElementById("input_bin_id");
-input_bin.addEventListener('input', update_from_bin);
-var input_oct = document.getElementById("input_oct_id");
-input_oct.addEventListener('input', update_from_oct);
-var input_dec = document.getElementById("input_dec_id");
-input_dec.addEventListener('input', update_from_dec);
-var input_hex = document.getElementById("input_hex_id");
-input_hex.addEventListener('input', update_from_hex);
-var input_ncard = document.getElementById("input_ncard_id");
-input_ncard.addEventListener('input', update_from_ncard);
+// 検索キーワード
+var check_search_keyword = document.getElementById("id_check_search-keyword");
+check_search_keyword.addEventListener('input', update_search_keyword);
+var input_search_keyword = document.getElementById("id_input_search-keyword");
+input_search_keyword.addEventListener('input', update_search_keyword);
 
-var input_n = document.getElementById("input_n_id");
-input_n.addEventListener('input', update_n);
-var n = "";
+var search_keyword = "";
 
-var output_bin = document.getElementById("output_bin_id");
-var output_oct = document.getElementById("output_oct_id");
-var output_dec = document.getElementById("output_dec_id");
-var output_hex = document.getElementById("output_hex_id");
-var output_ncard = document.getElementById("output_ncard_id");
-
-var dec_num = "";
-
-function update_from_bin(e) {
-    dec_num = parseInt(input_bin.value, 2);
-
-    input_oct.value = "";
-    input_dec.value = "";
-    input_hex.value = "";
-    input_ncard.value = "";
-
-    update_outputs(dec_num);
-}
-
-function update_from_oct(e) {
-    dec_num = parseInt(input_oct.value, 8);
-
-    input_bin.value = "";
-    input_dec.value = "";
-    input_hex.value = "";
-    input_ncard.value = "";
-
-    update_outputs(dec_num);
-}
-
-function update_from_dec(e) {
-    dec_num = parseInt(input_dec.value, 10);
-
-    input_bin.value = "";
-    input_oct.value = "";
-    input_hex.value = "";
-    input_ncard.value = "";
-
-    update_outputs(dec_num);
-}
-
-function update_from_hex(e) {
-    dec_num = parseInt(input_hex.value, 16);
-
-    input_bin.value = "";
-    input_oct.value = "";
-    input_dec.value = "";
-    input_ncard.value = "";
-
-    update_outputs(dec_num);
-}
-
-function update_from_ncard(e) {
-    var ncard_num = parseInt(input_ncard.value, parseInt(n, 10));
-
-    input_bin.value = "";
-    input_oct.value = "";
-    input_dec.value = "";
-    input_hex.value = "";
-
-    update_outputs(ncard_num);
-}
-
-function update_n(e) {
-    n = document.getElementById("input_n_id").value;
-
-    update_outputs(dec_num);
-}
-
-function update_outputs(dec_num) {
-    output_bin.value = dec_num.toString(2);
-    output_oct.value = dec_num.toString(8);
-    output_dec.value = dec_num.toString(10);
-    output_hex.value = dec_num.toString(16);
-    
-    var n = document.getElementById("input_n_id").value;
-    if (isNaN(parseInt(n, 10)) == false) {
-        output_ncard.value = dec_num.toString(parseInt(n, 10));
+function update_search_keyword() {
+    if (check_search_keyword.checked) {
+        search_keyword = input_search_keyword.value;
     }
+    else {
+        search_keyword = "";
+    }
+
+    update();
+}
+
+// 出力の更新
+var output = document.getElementById("id_output");
+var output_cmd = "";
+function update() {
+    output_cmd = `${search_keyword}`;
+
+    output.value = output_cmd;
 }
