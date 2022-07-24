5ce744f38d9940b2ff2357b22940f30a8ddfa6e4
YotioSoft <ytani0323@gmail.com>
2022-07-25 01:03:19 +0900

    add output: without-br

--- char-counter/js/char-counter.js
+++ char-counter/js/char-counter.js
@@ -1,12 +1,16 @@
 $(window).on('load', function() {
     $('#input-textarea').keyup(function() {
         var output_char_count = document.getElementById("output-char-count");
+        var output_char_count_wo_br = document.getElementById("output-char-count-without-br");
+
         var input_textarea = document.getElementById("input-textarea");
 
         // 各カウント値の取得
         var char_count = input_textarea.value.length;
+        var char_count_wo_br = input_textarea.value.replace(/\r\n|\r|\n/g, "").length;
 
         // 出力
         output_char_count.value = char_count;
+        output_char_count_wo_br.value = char_count_wo_br;
     });
 });
