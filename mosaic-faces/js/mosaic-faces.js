781e1c4e3a32641e4456897aa30e782737c29cba
YotioSoft <ytani0323@gmail.com>
2022-09-05 20:14:10 +0900

    allows image input as many times as required

--- mosaic-faces/js/mosaic-faces.js
+++ mosaic-faces/js/mosaic-faces.js
@@ -1,8 +1,11 @@
 // 入力画像の描画処理
-function drawMapEasy(image) {
+function drawImage(image) {
     // 仮想キャンバスに画像を描画（画像サイズはそのまま）
     virtual_canvas = document.querySelector('#virtual-canvas');
     virtual_ctx = virtual_canvas.getContext('2d');
+    // 一旦クリア
+    virtual_ctx.clearRect(0, 0, virtual_canvas.width, virtual_canvas.height);
+    // 画像を描画
     virtual_canvas.width = image.width;
     virtual_canvas.height = image.height;
     virtual_ctx.drawImage(image, 0, 0, image.width, image.height);
@@ -15,6 +18,9 @@ function drawMapEasy(image) {
 
     // リサイズ処理（最大: x=1000, y=1000）
     ctx = canvas_input.getContext('2d');
+    // 一旦クリア
+    ctx.clearRect(0, 0, canvas_input.width, canvas_input.height);
+    // 画像を描画
     img = image;
     let width = img.width, height = img.height;
     const max_width = 1000, max_height = 1000;
@@ -46,6 +52,13 @@ function drawMapEasy(image) {
     ctx.canvas.width = width;
     ctx.canvas.height = height;
     ctx.drawImage(image, 0, 0, width, height);
+
+    /*--------------------------------------------------*/
+
+    // 出力用キャンバスをクリア
+    canvas_output = document.querySelector('#img-output');
+    ctx_output = canvas_output.getContext('2d');
+    ctx_output.clearRect(0, 0, canvas_output.width, canvas_output.height);
 }
 
 // cvが読み込まれたときに実行
@@ -98,89 +111,80 @@ function mosaic(img, x, y, w, h) {
 
 // utils.jsが読み込まれたときに実行
 function onUtilsLoaded() {
-    // cascadeファイルの読み込み
-    let faceCascade = new cv.CascadeClassifier();
-
     // ファイル入力
     let fileInput = document.getElementById('fileInput');
 
-    // 学習済みデータの読み込み
+    // 学習済みデータの読み込みの準備
     // xmlファイルを読み込むので、utilsで読み込んでからcascadeを読み込む必要あり
     faceCascadeFile = './haarcascade_frontalface_default.xml';
     const utils = new Utils('error-message');
     utils.createFileFromUrl(faceCascadeFile, faceCascadeFile, () => {
-        faceCascade.load(faceCascadeFile);
+        // ファイル準備完了時の動作
+        fileInput.onchange = (e) => {
+            // 画像読み込み準備
+            const image = new Image();
+            image.src = URL.createObjectURL(e.target.files[0]);
+
+            image.onload = ()  => {
+                // 画像をimg-inputキャンバスに読み込み
+                drawImage(image);
+
+                // 学習済みデータの読み込み
+                let faceCascade = new cv.CascadeClassifier();
+                faceCascade.load(faceCascadeFile);
+                detect(faceCascade);
+            }
+        };
     });
-        
-    // ファイル読み込み完了時の動作
-    fileInput.onchange = (e) => {
-        // 画像読み込み準備
-        const image = new Image();
-        image.src = URL.createObjectURL(e.target.files[0]);
+}
 
-        image.onload = ()  => {
-            // 画像をimg-inputキャンバスに読み込み
-            drawMapEasy(image)
+function detect(faceCascade) {
+    // 読み込み完了後：
+    // img-inputキャンパスからopencvに読み込み
+    let cvImage = cv.imread("img-input");
+    let img_width = cvImage.cols;
+    let img_height = cvImage.rows;
+
+    // グレースケール化
+    let gray = new cv.Mat();
+    cv.cvtColor(cvImage, gray, cv.COLOR_RGBA2GRAY, 0);
             
-            // 読み込み完了後：
-            // img-inputキャンパスからopencvに読み込み
-            let cvImage = cv.imread("img-input");
-            let img_width = cvImage.cols;
-            let img_height = cvImage.rows;
+    // 顔検出
+    let faces = new cv.RectVector();
+    let msize = new cv.Size(0, 0);
+    faceCascade.detectMultiScale(gray, faces, 1.1, 3, 0, msize, msize);
 
-            // グレースケール化
-            let gray = new cv.Mat();
-            cv.cvtColor(cvImage, gray, cv.COLOR_RGBA2GRAY, 0);
-            
-            // 顔検出
-            let faces = new cv.RectVector();
-            let msize = new cv.Size(0, 0);
-            faceCascade.detectMultiScale(gray, faces, 1.1, 3, 0, msize, msize);
+    console.log("faces.size() = " + faces.size());
 
-            console.log("faces.size() = " + faces.size());
+    // 検出した領域に赤枠を表示
+    for (let i = 0; i < faces.size(); ++i) {
+        mosaic(cvImage, faces.get(i).x, faces.get(i).y, faces.get(i).width, faces.get(i).height);
+    }
 
-            // 検出した領域に赤枠を表示
-            for (let i = 0; i < faces.size(); ++i) {
-                //mosaic_area(cvImage, faces.get(i).x, faces.get(i).y, faces.get(i).width, faces.get(i).height);
-                mosaic(cvImage, faces.get(i).x, faces.get(i).y, faces.get(i).width, faces.get(i).height);
-                /*
-                let point1 = new cv.Point(faces.get(i).x, faces.get(i).y);
-                let point2 = new cv.Point(faces.get(i).x + faces.get(i).width, faces.get(i).y + faces.get(i).height);
-                cv.rectangle(cvImage, point1, point2, [255, 0, 0, 255]);
-                */
-            }
+    // 顔検出結果をimg-outputキャンバスに表示
+    // output用のキャンバスも同じサイズにする
+    canvas_output = document.querySelector('#img-output');
+    ctx_output = canvas_output.getContext('2d');
+    canvas_output.width = img_width;
+    canvas_output.height = img_height;
+    canvas_output.style.width = img_width + "px";
+    canvas_output.style.height = img_height + "px";
 
-            // 顔検出結果をimg-outputキャンバスに表示
-            // output用のキャンバスも同じサイズにする
-            canvas_output = document.querySelector('#img-output');
-            ctx_output = canvas_output.getContext('2d');
-            canvas_output.width = img_width;
-            canvas_output.height = img_height;
-            canvas_output.style.width = img_width + "px";
-            canvas_output.style.height = img_height + "px";
+    cv.imshow("img-output", cvImage);
 
-            cv.imshow("img-output", cvImage);
+    // 仮想キャンバスにも適用（ダウンロード用; サイズは元画像と同じ）
+    let virtualImage = cv.imread("virtual-canvas");
+    virtual_canvas = document.querySelector('#virtual-canvas');
+    for (let i = 0; i < faces.size(); ++i) {
+        mosaic(virtualImage, faces.get(i).x * (virtual_canvas.width / img_width), faces.get(i).y * (virtual_canvas.width / img_width),
+                    faces.get(i).width * (virtual_canvas.width / img_width), faces.get(i).height * (virtual_canvas.width / img_width));
+    }
+    cv.imshow("virtual-canvas", virtualImage);
 
-            // 仮想キャンバスにも適用（ダウンロード用; サイズは元画像と同じ）
-            let virtualImage = cv.imread("virtual-canvas");
-            virtual_canvas = document.querySelector('#virtual-canvas');
-            for (let i = 0; i < faces.size(); ++i) {
-                mosaic(virtualImage, faces.get(i).x * (virtual_canvas.width / img_width), faces.get(i).y * (virtual_canvas.width / img_width),
-                         faces.get(i).width * (virtual_canvas.width / img_width), faces.get(i).height * (virtual_canvas.width / img_width));
-                /*
-                let point1 = new cv.Point(faces.get(i).x * (virtual_canvas.width / img_width), faces.get(i).y * (virtual_canvas.height / img_height));
-                let point2 = new cv.Point((faces.get(i).x + faces.get(i).width) * (virtual_canvas.width / img_width), (faces.get(i).y + faces.get(i).height) * (virtual_canvas.height / img_height));
-                cv.rectangle(virtualImage, point1, point2, [255, 0, 0, 255]);
-                */
-            }
-            cv.imshow("virtual-canvas", virtualImage);
-
-            // メモリ解放
-            cvImage.delete();
-            virtualImage.delete();
-            gray.delete();
-            faces.delete();
-            faceCascade.delete();
-        }
-    };
+    // メモリ解放
+    cvImage.delete();
+    virtualImage.delete();
+    gray.delete();
+    faces.delete();
+    faceCascade.delete();
 }
