5478611b3b39c590322be51fbf5e75b22f72ca46
yotiosoft <yotio@yotiosoft.com>
2024-03-10 11:59:01 +0900

    base64

--- base64/js/base64.js
+++ base64/js/base64.js
@@ -1,11 +1,9 @@
-var input_text = null;
+var base64_data = "";
 
 function OnEncodeButtonClick() {
     var input_textarea = document.getElementById("input-textarea");
     var output_textarea = document.getElementById("output-textarea");
-    if (input_text == null) {
-        input_text = input_textarea.value;
-    }
+    input_text = input_textarea.value;
     var result = base64_encode(input_text);
     output_textarea.value = result;
 }
@@ -18,20 +16,44 @@ function OnDecodeButtonClick() {
     output_textarea.value = result;
 }
 
-function DropHandler(event) {
+function OnDownloadButtonClick() {
+    var text = base64_data;
+    console.log(text);
+    var blob = new Blob([text], { type: "application/octet-stream" });
+    var url = URL.createObjectURL(blob);
+    var a = document.createElement("a");
+    a.href = url;
+    a.download = "base64.png";
+    a.click();
+}
+
+function dropHandler(event) {
     event.preventDefault();
-    var input_textarea = document.getElementById("input-textarea");
     var output_textarea = document.getElementById("output-textarea");
     var file = event.dataTransfer.files[0];
     var reader = new FileReader();
     reader.onload = function (e) {
-        input_textarea.value = e.target.result;
         var result = base64_encode(e.target.result);
         output_textarea.value = result;
     };
     reader.readAsText(file);
-    input_text = reader.result;
-    console.log(input_text);
+}
+
+function dragOverHandler(event) {
+    event.preventDefault();
+    var drop_area_wrap = document.getElementById("file-drop");
+    drop_area_wrap.style.border = "2px dashed #000";
+}
+
+function fileInputHandler(event) {
+    var output_textarea = document.getElementById("output-textarea");
+    var file = event.target.files[0];
+    var reader = new FileReader();
+    reader.onload = function (e) {
+        var result = base64_encode(e.target.result);
+        output_textarea.value = result;
+    };
+    reader.readAsText(file);
 }
 
 function base64_encode(str) {
@@ -39,5 +61,6 @@ function base64_encode(str) {
 }
 
 function base64_decode(str) {
-    return decodeURIComponent(escape(window.atob(str)));
+    base64_data = decodeURIComponent(escape(window.atob(str)));
+    return base64_data;
 }
