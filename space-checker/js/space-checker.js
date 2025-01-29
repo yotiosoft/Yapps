57ea22df58004f974a4bb43914cd7276f43fd0d5
yotiosoft <yotio@yotiosoft.com>
2025-01-30 02:38:30 +0900

    check full-space chars

--- space-checker/js/space-checker.js
+++ space-checker/js/space-checker.js
@@ -6,7 +6,7 @@ $('#input-textarea').keyup(function() {
         output.removeChild(output.firstChild);
     }
 
-    var tab_char = false, space_char = false;
+    var tab_char = false, half_space_char = false, full_space_char = false;
     var bcg = document.createElement("span");
     for (var i=0; i<input.value.length; i++) {
         // タブ文字
@@ -16,33 +16,50 @@ $('#input-textarea').keyup(function() {
                     output.appendChild(bcg);
 
                 tab_char = true;
-                space_char = false;
+                half_space_char = false;
+                full_space_char = false;
                 bcg = document.createElement("span");
                 bcg.style = "background-color: #ef454a;";
             }
             bcg.textContent += "\t";
         }
-        // スペース
-        else if (input.value[i].match(/^\s$/)) {
-            if (!space_char) {
+        // 半角スペース
+        else if (input.value[i].match(/^ $/)) {
+            if (!half_space_char) {
                 if (bcg != null)
                     output.appendChild(bcg);
 
-                space_char = true;
+                half_space_char = true;
+                full_space_char = false;
                 tab_char = false;
                 bcg = document.createElement("span");
-                bcg.style = "background-color: #ffcc00;";
+                bcg.style = "background-color: #20f582;";
+            }
+            bcg.textContent += input.value[i];
+        }
+        // 全角スペース
+        else if (input.value[i].match(/^　$/)) {
+            if (!full_space_char) {
+                if (bcg != null)
+                    output.appendChild(bcg);
+
+                full_space_char = true;
+                half_space_char = false;
+                tab_char = false;
+                bcg = document.createElement("span");
+                bcg.style = "background-color: #20c8f5;";
             }
             bcg.textContent += input.value[i];
         }
         // 通常文字
         else {
-            if (tab_char || space_char) {
+            if (tab_char || half_space_char || full_space_char) {
                 if (bcg != null)
                     output.appendChild(bcg);
 
                 tab_char = false;
-                space_char = false;
+                half_space_char = false;
+                full_space_char = false;
                 bcg = document.createElement("span");
             }
             bcg.textContent += input.value[i];
