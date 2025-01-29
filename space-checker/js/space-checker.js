934509c68a3290f8009ddf2503d6541611e60f63
yotiosoft <yotio@yotiosoft.com>
2025-01-30 02:17:58 +0900

    check tab characters

--- space-checker/js/space-checker.js
+++ space-checker/js/space-checker.js
@@ -6,26 +6,43 @@ $('#input-textarea').keyup(function() {
         output.removeChild(output.firstChild);
     }
 
-    var full_char = false;
+    var tab_char = false, space_char = false;
     var bcg = document.createElement("span");
     for (var i=0; i<input.value.length; i++) {
-        if (input.value[i].match(/^\s$/)) {
-            if (!full_char) {
+        // タブ文字
+        if (input.value[i].match(/^\t$/)) {
+            if (!tab_char) {
                 if (bcg != null)
                     output.appendChild(bcg);
 
-                full_char = true;
+                tab_char = true;
+                space_char = false;
+                bcg = document.createElement("span");
+                bcg.style = "background-color: #ef454a;";
+            }
+            bcg.textContent += "\t";
+        }
+        // スペース
+        else if (input.value[i].match(/^\s$/)) {
+            if (!space_char) {
+                if (bcg != null)
+                    output.appendChild(bcg);
+
+                space_char = true;
+                tab_char = false;
                 bcg = document.createElement("span");
                 bcg.style = "background-color: #ffcc00;";
             }
             bcg.textContent += input.value[i];
         }
+        // 通常文字
         else {
-            if (full_char) {
+            if (tab_char || space_char) {
                 if (bcg != null)
                     output.appendChild(bcg);
 
-                full_char = false;
+                tab_char = false;
+                space_char = false;
                 bcg = document.createElement("span");
             }
             bcg.textContent += input.value[i];
