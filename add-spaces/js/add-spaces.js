912b525a0b5dd5d814e5427f51b21d5af70e63b6
YotioSoft <ytani0323@gmail.com>
2025-01-30 12:16:50 +0900

    add without-marks checkbox

--- add-spaces/js/add-spaces.js
+++ add-spaces/js/add-spaces.js
@@ -1,19 +1,32 @@
 $('#input-textarea').keyup(convert);
-$('#replace_to_space').change(convert);
+$('#without-marks').change(convert);
+
+function insertSpacesWithoutMarks(text) {
+    return text.replace(/(?![、。（）「」『』])([\u3000-\u9FFF])([\u0020-\u007D]+)/gu, '$1 $2')
+               .replace(/([\u0020-\u007D]+)(?![、。（）「」『』])([\u3000-\u9FFF])/gu, '$1 $2');
+}
 
 function insertSpaces(text) {
-    return text.replace(/([\u3000-\u9FFF])([a-zA-Z0-9.,-/:-@\[-~\]]+)/gu, '$1 $2')
-               .replace(/([a-zA-Z0-9.,-/:-@\[-~\]]+)([\u3000-\u9FFF])/gu, '$1 $2');
+    return text.replace(/([\u3000-\u9FFF])([\u0020-\u007D]+)/gu, '$1 $2')
+               .replace(/([\u0020-\u007D]+)([\u3000-\u9FFF])/gu, '$1 $2');
 }
 
 function convert() {
     input_area  = document.getElementById("input-textarea");
     output_area = document.getElementById("output-textarea");
 
-    // 英単語と全角文字の間にスペースを入れる
-    var converted_str = insertSpaces(input_area.value);
+    // 句読点および括弧の前後にスペースを入れない
+    var without_marks = document.getElementById("without-marks").checked;
 
-    output_area.value = converted_str;
+    // 英単語と全角文字の間にスペースを入れる
+    if (without_marks) {
+        var converted_str = insertSpacesWithoutMarks(input_area.value);
+        output_area.value = converted_str;
+    }
+    else {
+        var converted_str = insertSpaces(input_area.value);
+        output_area.value = converted_str;
+    }
 }
 
 function OnCopyButtonClick() {
