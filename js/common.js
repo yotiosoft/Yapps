cbffb98780310a71c33b55e7d3d59a1faf54b626
YotioSoft <ytani0323@gmail.com>
2025-02-09 16:06:40 +0900

    impl apps.json

--- js/common.js
+++ js/common.js
@@ -52,3 +52,51 @@ function close_information_button(config_name, config_id) {
 
     $('#' + config_id).hide();
 }
+
+// /apps.json からアプリ情報を取得
+function get_apps_json(id) {
+    $.getJSON("apps.json", function(data) {
+        /* テンプレート:
+        <div class="box-wrap">
+            <a href="./space-checker/">
+                <div class="box">
+                    <img src="/img/tools_icon/text/space-checker.svg" type=”image/svg+xml” width="128" height="128" alt="空白文字チェッカー">
+                    <p class="tool-title">空白文字チェッカー</p>
+                    <p class="summary">文章中の空白文字を検出します。</p>
+                </div>
+            </a>
+        </div>
+        */
+       let apps_list = data.apps;
+       console.log(apps_list.length);
+        for (var i = 0; i < apps_list.length; i++) {
+            var app = apps_list[i];
+            var box_wrap = document.createElement("div");
+            box_wrap.className = "box-wrap";
+            var a = document.createElement("a");
+            a.href = app.path;
+            var box = document.createElement("div");
+            box.className = "box";
+            var img = document.createElement("img");
+            img.src = app.image;
+            img.width = 128;
+            img.height = 128;
+            img.alt = app.name;
+            var p1 = document.createElement("p");
+            p1.className = "tool-title";
+            p1.textContent = app.name;
+            var p2 = document.createElement("p");
+            p2.className = "summary";
+            p2.textContent = app.summary;
+
+            box.appendChild(img);
+            box.appendChild(p1);
+            box.appendChild(p2);
+            a.appendChild(box);
+            box_wrap.appendChild(a);
+            console.log(box_wrap);
+            document.getElementById(id).appendChild(box_wrap);
+        }
+    }
+    );
+}
