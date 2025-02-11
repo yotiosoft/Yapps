e249cbcdb4854467f9d6247483c30b7d85e626b7
YotioSoft <ytani0323@gmail.com>
2025-02-11 16:55:59 +0900

    responsible index page

--- js/common.js
+++ js/common.js
@@ -6,7 +6,7 @@ gtag('config', 'G-43EGG0HL47');
 
 //共通パー�?読み込み
 $(function() {
-    if (window.innerWidth <= 700 || navigator.userAgent.match(/iPhone|Android.+Mobile/)) {
+    if (window.innerWidth <= 768 || navigator.userAgent.match(/iPhone|Android.+Mobile/)) {
         $("#header").load("/header_m.html");
     }
     else {
@@ -15,7 +15,7 @@ $(function() {
     $("#footer").load("/footer.html");
     
     window.addEventListener('resize', function(){
-        if (window.innerWidth <= 700 || navigator.userAgent.match(/iPhone|Android.+Mobile/)) {
+        if (window.innerWidth <= 768 || navigator.userAgent.match(/iPhone|Android.+Mobile/)) {
             $("#header").load("/header_m.html");
         }
         else {
