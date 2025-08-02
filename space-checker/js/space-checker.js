7f94eb4605d4db7bb224109e31891d2b58d4fe9e
yotiosoft <yotio@yotiosoft.com>
2025-08-03 04:52:35 +0900

    hmm

--- space-checker/js/space-checker.js
+++ space-checker/js/space-checker.js
@@ -1,70 +1,50 @@
-$('#input-textarea').keyup(function() {
-    var input  = document.getElementById("input-textarea");
-    var output = document.getElementById("output-area");
+$('#input-textarea').on('input', function () {
+  const input = this.value;
+  const output = document.getElementById("output-area");
+  output.innerHTML = '';
 
-    while (output.firstChild) {
-        output.removeChild(output.firstChild);
+  let tab_char = false, half_space_char = false, full_space_char = false;
+  let bcg = document.createElement("span");
+
+  for (let i = 0; i < input.length; i++) {
+    const ch = input[i];
+
+    if (ch === "\t") {
+      if (!tab_char) {
+        if (bcg.textContent) output.appendChild(bcg);
+        tab_char = true; half_space_char = false; full_space_char = false;
+        bcg = document.createElement("span");
+        bcg.className = "tab";
+      }
+      bcg.textContent += "\t";
     }
-
-    var tab_char = false, half_space_char = false, full_space_char = false;
-    var bcg = document.createElement("span");
-    for (var i=0; i<input.value.length; i++) {
-        // タブ文字
-        if (input.value[i].match(/^\t$/)) {
-            if (!tab_char) {
-                if (bcg != null)
-                    output.appendChild(bcg);
-
-                tab_char = true;
-                half_space_char = false;
-                full_space_char = false;
-                bcg = document.createElement("span");
-                bcg.style = "background-color: #ef454a;";
-            }
-            bcg.textContent += "\t";
-        }
-        // 半角スペース
-        else if (input.value[i].match(/^ $/)) {
-            if (!half_space_char) {
-                if (bcg != null)
-                    output.appendChild(bcg);
-
-                half_space_char = true;
-                full_space_char = false;
-                tab_char = false;
-                bcg = document.createElement("span");
-                bcg.style = "background-color: #20f582;";
-            }
-            bcg.textContent += input.value[i];
-        }
-        // 全角スペース
-        else if (input.value[i].match(/^　$/)) {
-            if (!full_space_char) {
-                if (bcg != null)
-                    output.appendChild(bcg);
-
-                full_space_char = true;
-                half_space_char = false;
-                tab_char = false;
-                bcg = document.createElement("span");
-                bcg.style = "background-color: #20c8f5;";
-            }
-            bcg.textContent += input.value[i];
-        }
-        // 通常文字
-        else {
-            if (tab_char || half_space_char || full_space_char) {
-                if (bcg != null)
-                    output.appendChild(bcg);
-
-                tab_char = false;
-                half_space_char = false;
-                full_space_char = false;
-                bcg = document.createElement("span");
-            }
-            bcg.textContent += input.value[i];
-        }
-        if (bcg != null)
-            output.appendChild(bcg);
+    else if (ch === " ") {
+      if (!half_space_char) {
+        if (bcg.textContent) output.appendChild(bcg);
+        half_space_char = true; full_space_char = false; tab_char = false;
+        bcg = document.createElement("span");
+        bcg.className = "half-space";
+      }
+      bcg.textContent += " ";
     }
+    else if (ch === "　") {
+      if (!full_space_char) {
+        if (bcg.textContent) output.appendChild(bcg);
+        full_space_char = true; half_space_char = false; tab_char = false;
+        bcg = document.createElement("span");
+        bcg.className = "full-space";
+      }
+      bcg.textContent += ch;
+    }
+    else {
+      if (tab_char || half_space_char || full_space_char) {
+        if (bcg.textContent) output.appendChild(bcg);
+        tab_char = half_space_char = full_space_char = false;
+        bcg = document.createElement("span");
+      }
+      bcg.textContent += ch;
+    }
+  }
+
+  if (bcg.textContent) output.appendChild(bcg);
 });
