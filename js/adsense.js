1e264cb47673c1d4525b601d32ccef5db6bcb3de
yotiosoft <yotio@yotiosoft.com>
2026-05-10 21:46:48 +0900

    ads-infeed

--- js/adsense.js
+++ js/adsense.js
@@ -19,3 +19,26 @@ function ads_wide(id) {
     target.innerHTML = html;
     (adsbygoogle = window.adsbygoogle || []).push({});
 }
+
+function ads_infeed(id) {
+    const html = `
+<ins class="adsbygoogle"
+     style="display:block"
+     data-ad-format="fluid"
+     data-ad-layout-key="-hl+7-z-2d+86"
+     data-ad-client="ca-pub-5605897321593790"
+     data-ad-slot="2566021993"></ins>
+<script>
+     (adsbygoogle = window.adsbygoogle || []).push({});
+</script>
+`;
+
+    const target = document.getElementById(id);
+    if (!target) {
+        return;
+    }
+
+    target.innerHTML = html;
+    (adsbygoogle = window.adsbygoogle || []).push({});
+}
+
